13596575c3cfc0cc0931fd9d337950e3acbafa9c
Aaron D Borden <adborden@a14n.net>
2026-05-03 13:25:09 -0700

    fix: sh instead of bash

--- docker/run.sh
+++ docker/run.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 set -o errexit
-set -o pipefail
+#set -o pipefail
 set -o nounset
 
 : "${ANSIBLE_PULL_ARGS:=}"
 
-ansible-pull --url "${ANSIBLE_PULL_REPOSITORY}" --checkout "${ANSIBLE_PULL_REF}" ${ANSIBLE_PULL_ARGS} "${ANSIBLE_PULL_PLAYBOOK}"
+exec ansible-pull --url "${ANSIBLE_PULL_REPOSITORY}" --checkout "${ANSIBLE_PULL_REF}" ${ANSIBLE_PULL_ARGS} "${ANSIBLE_PULL_PLAYBOOK}"
