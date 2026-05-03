8ba73277e7ccd1e72fc52450f657176c312bf4ce
Aaron D Borden <adborden@a14n.net>
2026-05-03 10:50:58 -0700

    feat: playbook for castle-pull

--- docker/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -o errexit
-set -o pipefail
-set -o nounset
-
-: "${ANSIBLE_PULL_ARGS:=}"
-
-ansible-pull --url "${ANSIBLE_PULL_REPOSITORY}" --checkout "${ANSIBLE_PULL_REF}" ${ANSIBLE_PULL_ARGS} "${ANSIBLE_PULL_PLAYBOOK}"
